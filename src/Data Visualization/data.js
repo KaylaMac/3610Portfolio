f9ed63a0c3ebd85cee2e5912dbf42dba501a44b9
KaylaMac <90443863+KaylaMac@users.noreply.github.com>
2022-03-22 15:35:24 -0400

    Add files via upload

---
+++ src/Data Visualization/data.js
@@ -0,0 +1,65 @@
+var c= document.getElementById("myCanvas");
+var ctx= c.getContext("2d");
+
+const barChart= new Chart(ctx,{
+  type: "bar",
+  data: {
+    labels: ['District 01', 'District 02', 'District 03', 'District 04', 'District 05', 'District 06', 'District 07', 'District 08', 'District 09', 'District 10'],
+     datasets: [{
+       label: "Enrollment",
+       data: [12367,60823,21962,14252,13170,25733,19717,31625,34518,56757
+],
+       backgroundColor: [
+                'rgba(255, 110, 142, 0.4)',
+                'rgba(54, 162, 235, 0.4)',
+                'rgba(245, 226, 106, 0.4)',
+                'rgba(75, 192, 192, 0.4)',
+                'rgba(153, 102, 255, 0.4)',
+                'rgba(255, 159, 64, 0.4)'
+            ]
+     }]
+  },
+   
+  options:{
+    plugins:{
+      title:{
+       display:true,
+       text: "Kayla McIntosh"
+     }
+    }
+  }
+  
+});
+
+
+var d= document.getElementById("canvasTwo");
+var ctx2= d.getContext("2d");
+const lineChart= new Chart(ctx2,{
+  type: "doughnut",
+  data: {
+    labels: ['District 01', 'District 02', 'District 03', 'District 04', 'District 05', 'District 06', 'District 07', 'District 08', 'District 09', 'District 10'],
+     datasets: [{
+       label: "Enrollment",
+       data: [12367,60823,21962,14252,13170,25733,19717,31625,34518,56757
+],
+       backgroundColor: [
+                'rgba(255, 110, 142, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(245, 226, 106, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ]
+     }]
+  },
+   
+  options:{
+    plugins:{
+      title:{
+       display:true,
+       text: "Kayla McIntosh"
+     }
+    }
+  }
+  
+});
