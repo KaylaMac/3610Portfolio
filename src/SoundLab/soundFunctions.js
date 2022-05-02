524809c5a231438cecf3634043fc4e739ba50df5
KaylaMac <90443863+KaylaMac@users.noreply.github.com>
2022-05-01 21:24:48 -0400

    Add files via upload

---
+++ src/SoundLab/soundFunctions.js
@@ -0,0 +1,28 @@
+var sax = document.getElementById("sax");
+var guitar = document.getElementById("guitar");
+var bass = document.getElementById("bass");
+var trumpet = document.getElementById("trumpet");
+var drums = document.getElementById("drums");
+var clap= document.getElementById("clap");
+var pianoOne= document.getElementById("piano1");
+var pianoTwo= document.getElementById("piano2");
+var pianoThree = document.getElementById("piano3");
+
+
+function playSax(){sax.play();}
+
+function playGuitar(){guitar.play();}
+
+function playBass(){bass.play();}
+
+function playTrumpet(){trumpet.play();}
+
+function playDrums(){drums.play();}
+
+function playClap(){clap.play();}
+
+function playOne(){pianoOne.play();}
+
+function playTwo(){pianoTwo.play();}
+
+function playThree(){pianoThree.play();}
